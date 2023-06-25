a11b7b25c4bafdf62d851b7c62352a1452fbfac6
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-06-25 16:30:18 +0200

    04-2621-Sleep.js

--- 04-2621-Sleep.js
+++ 04-2621-Sleep.js
@@ -1,6 +1,12 @@
 /*
 https://leetcode.com/problems/sleep/
 
+Easy
+
+Given a positive integer millis, 
+write an asynchronous function that sleeps for millis milliseconds. 
+It can resolve any value.
+
 Runtime 51 ms Beats 92.41%
 Memory 41.6 MB Beats 62.89%
 */
