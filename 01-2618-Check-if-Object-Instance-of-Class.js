46c08a32358fb6e3b384812f095f6f18af8ddacf
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-06-25 16:33:51 +0200

    01-2618-Check-if-Object-Instance-of-Class.js

--- 01-2618-Check-if-Object-Instance-of-Class.js
+++ 01-2618-Check-if-Object-Instance-of-Class.js
@@ -2,6 +2,16 @@
 /*
 https://leetcode.com/problems/check-if-object-instance-of-class/
 
+Medium
+
+Write a function that checks if a given value 
+is an instance of a given class or superclass. 
+For this problem, an object is considered an instance of a given class 
+if that object has access to that class's methods.
+
+There are no constraints on the data types that can be passed to the function. 
+For example, the value or the class could be undefined.
+
 Runtime 113 ms Beats 33.77%
 Memory 52.8 MB Beats 21.43%
 */
