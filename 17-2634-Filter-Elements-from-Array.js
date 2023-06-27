f00a127d12501bd7372009ea6e439c05d0edbae2
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-06-28 00:55:54 +0200

    17-2634-Filter-Elements-from-Array.js minor fix

--- 17-2634-Filter-Elements-from-Array.js
+++ 17-2634-Filter-Elements-from-Array.js
@@ -3,7 +3,8 @@ https://leetcode.com/problems/filter-elements-from-array/
 
 Easy
 
-Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+Given an integer array arr and a filtering function fn, 
+return a filtered array filteredArr.
 
 The fn function takes one or two arguments:
 
