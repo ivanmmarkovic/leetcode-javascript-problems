3836852e7300268ce74d90dbcb7643f874a7ec1c
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-06-28 00:55:30 +0200

    V

---
+++ 18-2635-Apply-Transform-Over-Each-Element-in-Array.js
@@ -0,0 +1,31 @@
+/*
+
+Easy
+
+Given an integer array arr and a mapping function fn, 
+return a new array with a transformation applied to each element.
+
+The returned array should be created such that 
+returnedArray[i] = fn(arr[i], i).
+
+Please solve it without the built-in Array.map method.
+
+Runtime 59 ms Beats 54.69%
+Memory 41.7 MB Beats 85.66%
+
+*/
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let r = [];
+
+    for(let i = 0; i < arr.length; i++){
+        r.push(fn(arr[i], i));
+    }
+
+    return r;
+};
